a84d1b3d70ef006a0536f45f78e624c43a72addd
Rajaraman <rajaraman.ks@gmail.com>
2015-10-31 17:51:21 +0530

    Add histroy location paramter to routes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 "use strict";
 
-window.$ = window.jQuery = require('jquery');
-
 var React = require('react');
 var Router = require('react-router');
 var routes = require('./routes');
 
-Router.run(routes, function(Handler) {
+Router.run(routes, Router.HistoryLocation, function(Handler) {
 	React.render(<Handler/>, document.getElementById('app'));
 });
