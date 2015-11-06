b70be415ed2980ca34942d853b050fedf9e3463f
Rajaraman <rajaraman.ks@gmail.com>
2015-11-06 13:06:08 +0530

    Pull the text input to a common control

--- src/components/author/authorForm.js
+++ src/components/author/authorForm.js
@@ -1,38 +1,29 @@
 "use strict";
 
-var React = require("react");
+var React = require('react');
+var Input = require('../common/textInput');
 
 var AuthorForm = React.createClass({
-	render: function() {
-		return (
-			<div>
-				<form>
-					<h1> Manage Author</h1>
-					<label htmlFor="firstName">First Name</label>
-					<input type="text"
-                           name = "firstName"
-                           className="form-control"
-                           placeholder="First Name"
-                           ref="firstName"
-						   onChange={this.props.onChange}
-                           value={this.props.author.firstName}/>
-                    <br/>
-                    <label htmlFor="lastName">Last Name</label>
-                    <input type="text"
-                           name = "lastName"
-                           className="form-control"
-                           placeholder="Last Name"
-                           ref="lastName"
-						   onChange={this.props.onChange}
-						   value={this.props.author.lastName} />
+    render: function () {
+        return (
+            <form>
+                <h1> Manage Author</h1>
+                <Input
+                    name="firstName"
+                    label="First Name"
+                    value={this.props.author.firstName}
+                    onChange={this.props.onChange}/>
 
-                    <br/>
+                <Input
+                    name="lastName"
+                    label="Last Name"
+                    value={this.props.author.lastName}
+                    onChange={this.props.onChange}/>
 
-                    <input type="submit" value="save" className="btn btn-default"/>
-                </form>
-			</div>
-			);
-	}
+                <input type="submit" value="save" className="btn btn-default"/>
+            </form>
+        );
+    }
 });
 
 module.exports = AuthorForm;
